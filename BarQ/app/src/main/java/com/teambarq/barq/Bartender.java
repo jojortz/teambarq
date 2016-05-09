2ccb5bb58a27d833d9cee06c98174cbada36d5ba
jojortz <ortiz.jroman@gmail.com>
2016-05-08 18:29:50 -0700

    added some gridview on the ShiftActivity, as well as a Bartender class

---
+++ BarQ/app/src/main/java/com/teambarq/barq/Bartender.java
@@ -0,0 +1,20 @@
+package com.teambarq.barq;
+
+/**
+ * Created by jojortz on 5/8/2016.
+ */
+public class Bartender {
+    String name;
+    int profilePic;
+
+    public Bartender(){
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int profilePic(){
+        return this.profilePic;
+    }
+}
---
+++ BarQ/app/src/main/java/com/teambarq/barq/MyAdapter.java
@@ -0,0 +1,51 @@
+package com.teambarq.barq;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+        import java.util.ArrayList;
+
+/**
+ * Created by jojortz on 4/6/2016.
+ */
+public class MyAdapter extends ArrayAdapter<Bartender>{
+    Context context;
+    private static LayoutInflater inflater;
+
+    public MyAdapter (Context context, ArrayList<Bartender> bartenderList){
+        super(context, 0, bartenderList);
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    static class ViewHolder{
+        public TextView name;
+        public ImageView image;
+    }
+
+    @Override
+    public View getView(int pos, View cV, ViewGroup par) {
+        View row = cV;
+        //Get the data item for this position
+        Bartender bartender= getItem(pos);
+
+//Check if view exists and only created if not
+        if (row == null) {
+            //inflate code
+            row = inflater.inflate(R.layout.bartender_grid,null);
+            ViewHolder holder = new ViewHolder();
+            holder.image = (ImageView) row.findViewById(R.id.bartender_imageView);
+            holder.name = (TextView) row.findViewById(R.id.bartender_textView);
+            row.setTag(holder);
+        }
+        ViewHolder h = (ViewHolder) row.getTag();
+        h.image.setImageResource(bartender.profilePic);
+        h.name.setText(bartender.name);
+
+        return row;
+
+    }
+}
