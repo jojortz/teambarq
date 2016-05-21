c2d161519b5f6a2f20c1008202e10a9901a9e239
jojortz <ortiz.jroman@gmail.com>
2016-05-21 15:47:47 -0700

    Added navigation drawer to Analytics Activity

--- BarQ/app/src/main/java/com/teambarq/barq/AnalyticsActivity.java
+++ BarQ/app/src/main/java/com/teambarq/barq/AnalyticsActivity.java
@@ -50,6 +50,16 @@ public class AnalyticsActivity extends AppCompatActivity {
     private ArrayList<String> xAxisBarLabel = new ArrayList<>();
     private BarDataSet barDataSet;
     private BarChart chart;
+
+    //Navigation drawer
+    private DrawerLayout mDrawerLayout;
+    private RecyclerView navRecyclerView;
+    RecyclerView.Adapter navAdapter;                        // Declaring Adapter For Recycler View
+    RecyclerView.LayoutManager mLayoutManager;            // Declaring Layout Manager as a linear layout manager
+
+    private String drawerTitles[] = { "Analytics", "Shift Creator", "Serve" };
+    private int drawerIcons[] = {R.drawable.ic_analytics_icon,R.drawable.ic_add_person,R.drawable.ic_bar_icon};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,6 +75,54 @@ public class AnalyticsActivity extends AppCompatActivity {
 
         //get data
         getDataSet();
+
+        //Setting up navigation drawer
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.analytics_drawerLayout);
+
+        navRecyclerView = (RecyclerView) findViewById(R.id.navRecyclerView); // Assigning the RecyclerView Object to the xml View
+
+        navRecyclerView.setHasFixedSize(true);                            // Letting the system know that the list objects are of fixed size
+
+        navAdapter = new NavAdapter(drawerTitles, drawerIcons, "BarQ", "BarQ@gmail.com", R.drawable.bar_icon);       // Creating the Adapter of MyAdapter class(which we are going to see in a bit)
+        // And passing the titles,icons,header view name, header view email,
+        // and header view profile picture
+        navRecyclerView.setAdapter(navAdapter);                              // Setting the adapter to RecyclerView
+        mLayoutManager = new LinearLayoutManager(this);                 // Creating a layout Manager
+        navRecyclerView.setLayoutManager(mLayoutManager);
+        mLayoutManager = new LinearLayoutManager(this);                 // Creating a layout Manager
+        navRecyclerView.setLayoutManager(mLayoutManager);                 // Setting the layout Manager
+
+        //Adding touch listener for RecycleView items
+        navRecyclerView.addOnItemTouchListener(
+                new RecyclerItemClickListener(getApplicationContext(), new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        if (position == 1)
+                        {
+                            //Close navigation drawer
+                            mDrawerLayout.closeDrawers();
+                        }
+                        else if(position == 2)
+                        {
+                            //Close navigation drawer
+                            mDrawerLayout.closeDrawers();
+
+                            //Launch Shift Activity
+                            Intent intent = new Intent(AnalyticsActivity.this, ShiftActivity.class);
+                            startActivity(intent);
+                        }
+                        else if(position == 3)
+                        {
+                            //Close navigation drawer
+                            mDrawerLayout.closeDrawers();
+
+                            //Launch Serve Activity
+                            Intent intent = new Intent(AnalyticsActivity.this, ServeActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                })
+        );
 
     }
 
